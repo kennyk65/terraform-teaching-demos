ba8f43310c6c7ae21904f684fbad90cce026b639
Ken Krueger <kenkrueger65@gmail.com>
2023-07-11 15:33:18 -0400

    Added peering

--- network-connections-demo/main.tf
+++ network-connections-demo/main.tf
@@ -1,3 +1,30 @@
+#  This template illustrates three separate ways to connect two VPCs,
+#  VPC Peering, Transit Gateway, and VPC (Interface) Endpoints.
+#  It launches EC2 instances in the provider and consumer VPCs to
+#  make the connectivity easier to demonstrate.  It also provides a 
+#  quick link to the network reachability page in the management console 
+#  to graphically show the network components.
+#
+#  To demonstrate VPC Endpoints / Private Link, create the stack with option 1.
+#  The provider instance hosts a small web service (Python/Flask in a Docker container)
+#  Which can be called via the VPC Endpoint's DNS name.  Find the VPC endpoint and copy 
+#  one of it's DNS names.  Use the session manager link to connect to the consumer instance.
+#  Call the web service like this:  curl http://<DNSNAME>/api?value=2 .
+#  
+#  To demonstrate VPC peering, create the stack with option 2.  Find the 
+#  private IP address of the provider instance.  Use
+#  the session manager link to connect to the consumer instance.  Ping the
+#  destination instance like this:   ping -c 6 <IP-ADDRESS>     or call the web service 
+#  like this:  curl http://<IP-ADDRESS>/api?value=2
+#
+#  To demonstrate Transit Gateway, create the stack with option 3.  Follow the
+#  same demo ideas for peering.
+#
+#  Optional: For any of these options, use the link in stack output to open
+#  VPC / Network Manager / Reachability Analyzer.  Enter source as consumer 
+#  instance, target as provider instance.
+
+
 terraform {
   required_providers {
     aws = {
@@ -41,6 +68,20 @@ variable "docker_image" {
     type = string
     default = "public.ecr.aws/kkrueger/flask-api:1"
     description = "Image to use for a container.  Can be from DockerHub or include registry URL for a different source (repository-url/image:tag)."
+}
+
+variable "connection_type" {
+    type = number
+    default = 1
+    description = "1=Private Link,  2=VPC Peering,  3=Transit Gateway"
+}
+
+# These variables allow us to selectively create private link, peering, 
+# or transit gateway resources depending on the demo being run:
+locals {
+  create_private_link    = (var.connection_type == 1 ? 1 : 0 )
+  create_peering         = (var.connection_type == 2 ? 1 : 0 )
+  create_transit_gateway = (var.connection_type == 3 ? 1 : 0 )
 }
 
 #  Provider network:
@@ -238,21 +279,46 @@ resource "aws_iam_role" "IAMRole" {
 }
 
 
+# PEERING STARTS HERE!!
+resource "aws_vpc_peering_connection" "peering_connection" {
+    count       = local.create_peering  # Only create this resource if we want to build a peering demo
+    peer_vpc_id = aws_vpc.ProviderVPC.id
+    vpc_id      = aws_vpc.ConsumerVPC.id
+}  
+
+resource "aws_route" "provider_peering_route" {
+    count                     = local.create_peering  # Only create this resource if we want to build a peering demo
+    vpc_peering_connection_id = aws_vpc_peering_connection.peering_connection[0].id
+    route_table_id            = aws_route_table.ProviderRouteTable.id
+    destination_cidr_block    = "172.16.0.0/16"
+}
+
+resource "aws_route" "consumer_peering_route" {
+    count                     = local.create_peering  # Only create this resource if we want to build a peering demo
+    vpc_peering_connection_id = aws_vpc_peering_connection.peering_connection[0].id
+    route_table_id            = aws_route_table.ConsumerRouteTable.id
+    destination_cidr_block    = "10.0.0.0/16"
+}
+
+
 #  VPC ENDPOINT STARTS HERE
 resource "aws_vpc_endpoint_service" "ConsumerEndpointService" {
-  acceptance_required        = false
-  network_load_balancer_arns = [aws_lb.NLB.arn]
+    count = local.create_private_link  # Only create this resource if we want to build a private link demo
+    acceptance_required        = false
+    network_load_balancer_arns = [aws_lb.NLB[0].arn]
 }
 
 resource "aws_vpc_endpoint" "EC2VPCEndpoint" {
+    count = local.create_private_link  # Only create this resource if we want to build a private link demo
     vpc_endpoint_type = "Interface"
     vpc_id = aws_vpc.ConsumerVPC.id
-    service_name = "com.amazonaws.vpce.${var.region}.${aws_vpc_endpoint_service.ConsumerEndpointService.id}"
+    service_name = "com.amazonaws.vpce.${var.region}.${aws_vpc_endpoint_service.ConsumerEndpointService[0].id}"
     subnet_ids = [ aws_subnet.ConsumerSubnet.id ]
     security_group_ids = [ aws_security_group.ConsumerSecurityGroup.id ]
 }
 
 resource "aws_lb" "NLB" {
+    count = local.create_private_link  # Only create this resource if we want to build a private link demo
     name = "NetworkLoadBalancer"
     internal = true
     load_balancer_type = "network"
@@ -260,6 +326,7 @@ resource "aws_lb" "NLB" {
 }
 
 resource "aws_lb_target_group" "TG" {
+    count = local.create_private_link  # Only create this resource if we want to build a private link demo
     name = "ProviderTargetGroup"
     port = 80
     protocol = "TCP"
@@ -268,34 +335,38 @@ resource "aws_lb_target_group" "TG" {
 }
 
 resource "aws_lb_target_group_attachment" "attachment" {
-  target_group_arn = aws_lb_target_group.TG.arn
-  target_id        = aws_instance.ProviderEC2Instance.id
-  port             = 80
+    count = local.create_private_link  # Only create this resource if we want to build a private link demo
+    target_group_arn = aws_lb_target_group.TG[0].arn
+    target_id        = aws_instance.ProviderEC2Instance.id
+    port             = 80
 }
 
 resource "aws_lb_listener" "Listener" {
-  load_balancer_arn = aws_lb.NLB.arn
-  port              = 80
-  protocol          = "TCP"
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.TG.arn
-  }
+    count = local.create_private_link  # Only create this resource if we want to build a private link demo
+    load_balancer_arn = aws_lb.NLB[0].arn
+    port              = 80
+    protocol          = "TCP"
+    default_action {
+        type             = "forward"
+        target_group_arn = aws_lb_target_group.TG[0].arn
+    }
 }
 
 # Outputs
 
 output "consumer_session_manager_link" {
-  description = "Access the service CONSUMER using web browser." 
-  value = "https://${var.region}.console.aws.amazon.com/systems-manager/session-manager/${aws_instance.ConsumerEC2Instance.id}?region=${var.region}#"  
+    description = "Access the service CONSUMER using web browser." 
+    value = "https://${var.region}.console.aws.amazon.com/systems-manager/session-manager/${aws_instance.ConsumerEC2Instance.id}?region=${var.region}#"  
 }
 
 output "reachability_analyzer_link" {
-  description = "Convenient link to the Network Manager Reachability Analyzer.  Enter 'Consumer' instance as source and 'Provider' instance as target"
-  value = "https://${var.region}.console.aws.amazon.com/networkinsights/home?region=${var.region}#CreateNetworkPath"
+    description = "Convenient link to the Network Manager Reachability Analyzer.  Enter 'Consumer' instance as source and 'Provider' instance as target"
+    value = "https://${var.region}.console.aws.amazon.com/networkinsights/home?region=${var.region}#CreateNetworkPath"
 }
 
-output "dns_name" {
-  description = "DNS Name of the VPC Endpoint."
-  value = element(aws_vpc_endpoint.EC2VPCEndpoint.dns_entry,0)
-}
+# TODO: CREATE DNS NAME CONDITIONALLY WHEN DOING A VPC ENDPOINT DEMO
+# output "dns_name" {
+#     count = local.create_private_link  # Only create this resource if we want to build a private link demo
+#     description = "DNS Name of the VPC Endpoint."
+#     value = element(aws_vpc_endpoint.EC2VPCEndpoint.dns_entry,0)
+# }
