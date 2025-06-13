6a83b962796aae0160fb23f6941c942c7e69ef07
Ken Krueger <kenkrueger65@gmail.com>
2025-06-13 17:10:56 -0400

    added sample app to deploy

---
+++ aws-deploy/main.tf
@@ -0,0 +1,26 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.80.0, < 6.0.0" # Not tested beyond v5.
+    }
+  }
+  backend "s3" {
+    bucket         = "kk-admin-terraform"
+    key            = "aws-deploy/default/terraform.tfstate"
+    region         = "us-west-2"
+    dynamodb_table = "terraform-lock"
+    encrypt        = true
+  }
+}
+
+
+provider "aws" {
+    region = "us-west-2"
+}
+
+# create an s3 bucket 
+resource "aws_s3_bucket" "temp-bucket" {
+    bucket = "kk-testing-github-action"
+}
